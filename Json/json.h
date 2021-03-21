f6485ebd32e0a2a4de3ad6410c408d2563f38805
Amirhossein Alizad <50969580+awwmiir@users.noreply.github.com>
2021-03-21 23:40:01 +0330

    changed Json files directory

---
+++ Json/json.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <algorithm>
+#include <vector>
+#include "../server/User.h"
+
+void break_data(int i);
+void find_user_data();
+void parse_files();
+void parse_users();
+void parse(std::string jason_data);
+std::string rmv_spc_newl(std::string str);
+void jsonParser();
+std::vector<User> userGetter();
+std::vector<std::string> filesGetter();
+std::string getCommandPort();
+std::string getDataPort();
